67a89f9cc22f7c01cd2d092e94accfdac8f20b14
fernanda <113780367+f3rnn@users.noreply.github.com>
2024-09-23 19:06:46 +0000

    .

--- projeto/models/pessoa.py
+++ projeto/models/pessoa.py
@@ -2,7 +2,7 @@ from projeto.models.endereco import Endereco
 
 class Pessoa:
     def __init__(self, id: int, nome: str, telefone: str, email: str, endereco: Endereco) -> None:
-        self.id = id
+        self.id = self.__verificar_id
         self.nome = nome
         self.telefone = telefone
         self.email = email
@@ -16,3 +16,10 @@ class Pessoa:
             f"\nemail: {self.email}"
             f"\nendereço: {self.endereco}"
         )
+    
+    def __verificar_id(self, id):
+        if id < 0:
+            raise ValueError("valor inválido")
+        if not isinstance(id, int):
+            raise TypeError("valor inválido")
+        return id
