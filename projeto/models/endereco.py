16d37ebe84e1eca18ce98f7a953913730068d3e0
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-23 13:46:53 +0000

    .

---
+++ projeto/models/endereco.py
@@ -0,0 +1,22 @@
+
+from projeto.models.enum.uf import UnidadeFederativa
+
+
+class Endereco:
+    def __init__(self,logradouro:str,numero:str,complemento:str,cep:str,cidade:str,uf:UnidadeFederativa) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+        self.uf = uf
+
+    def __str__(self) -> str:
+        return(
+            f"\nLogradouro: {self.logradouro}"
+            f"\nNúmero: {self.numero}"
+            f"\nComplemento: {self.complemento}"
+            f"\nCEP: {self.cep}"
+            f"\nCidade: {self.cidade}"
+            f"\nUF: {self.uf}"
+        )
