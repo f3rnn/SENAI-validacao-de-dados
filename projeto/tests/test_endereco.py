9a69a664301f664f55df0804263efc7d124cb791
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-23 19:48:03 +0000

    .

--- projeto/tests/test_endereco.py
+++ projeto/tests/test_endereco.py
@@ -10,5 +10,14 @@ def endereco_valido():
 def test_atributo_logradouro_valido(endereco_valido):
     assert endereco_valido.logradouro == "alameda"
 
+def test_atributo_numero_valido(endereco_valido):
+    assert endereco_valido.numero == "123"
+
+def test_atributo_complemento_valido(endereco_valido):
+    assert endereco_valido.complemento == "ali na esquina"
+
 def test_atributo_cep_valido(endereco_valido):
     assert endereco_valido.cep == "40.000-000"
+
+def test_atributo_cidade_valida(endereco_valido):
+    assert endereco_valido.cidade == "salvador"
