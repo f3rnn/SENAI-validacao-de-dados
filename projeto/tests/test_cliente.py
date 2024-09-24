dd607418a73a3b4eba89f1b1697c87f635fa5d68
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-24 13:54:22 +0000

    0.4

--- projeto/tests/test_cliente.py
+++ projeto/tests/test_cliente.py
@@ -13,4 +13,14 @@ def cliente_valido():
                     Endereco("alameda", "123", "ali na esquina", "40.000-000", "salvador", UnidadeFederativa.BAHIA),
                     Genero.MASCULINO,EstadoCivil.CASADO,"24/09/1999",1123)
 
- 
+def test_atributo_nome_valido(cliente_valido):
+    assert cliente_valido.nome == "José Tigrão"
+
+def test_atributo_id_valido(cliente_valido):
+    assert cliente_valido.id == 18
+
+def test_atributo_telefone_valido(cliente_valido):
+    assert cliente_valido.telefone == "9899-9999"
+
+def test_atributo_email_valido(cliente_valido):
+    assert cliente_valido.email == "bondedotigrão@gmail.com"
