596ba072b0f851757c6f65cf3e4c06d05e6d20ea
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-24 13:39:29 +0000

    0.3

--- projeto/tests/test_fornecedor.py
+++ projeto/tests/test_fornecedor.py
@@ -7,6 +7,6 @@ from projeto.models.fornecedores import Fornecedor
 
 @pytest.fixture
 def fornecedor_valido():
-        Fornecedor(18, "José Tigrão", "9899-9999", "bondedotigrão@gmail.com",
+        return Fornecedor(18, "José Tigrão", "9899-9999", "bondedotigrão@gmail.com",
                     Endereco("alameda", "123", "ali na esquina", "40.000-000", "salvador", UnidadeFederativa.BAHIA),
                     "68.582.878/0001-16","5277216-46","Hardware")
